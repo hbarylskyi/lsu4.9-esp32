6767d77b84a3f539c14be6dc1d75ef9e2515dfa6
hbarylskyi <hleb.barylskyi@gmail.com>
2025-09-20 02:14:09 +0300

    -

--- src/display.h
+++ src/display.h
@@ -8,6 +8,7 @@
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
 #define OLED_RESET    -1
+#define SSD1306_I2C_ADDRESS 0x3C
 
 class Display {
 public:
--- src/main.cpp
+++ src/main.cpp
@@ -15,12 +15,14 @@ void setup() {
 
 void loop() {
   Serial.println("Rendering static message on display...");
-  display.clearDisplay();
-  display.setTextSize(1);
-  display.setTextColor(SSD1306_WHITE);
-  display.setCursor(0, 0);
-  display.print("System Initialized");
-  display.display();
+  display.showData(14.7, 3000); // Example AFR and RPM values
+  Serial.println("Message rendered.");
+  // display.clearDisplay();
+  // display.setTextSize(1);
+  // display.setTextColor(SSD1306_WHITE);
+  // display.setCursor(0, 0);
+  // display.print("System Initialized");
+  // display.display();
   delay(5000); // Delay to keep the message on screen
 }
 
